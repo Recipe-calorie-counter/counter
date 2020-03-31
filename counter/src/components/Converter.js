9c77e7a6e4bb7f56c45ec9601d30b73c152bbb46
Aleksandra <a.orlovskaia@gmail.com>
2020-03-31 21:45:42 +0300

    one file without styles

--- counter/src/components/Converter.js
+++ counter/src/components/Converter.js
@@ -1,20 +1,45 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {
+    NavLink
+} from "react-router-dom";
 import styled from "styled-components";
+import FormConverter from './converter/FormConverter';
 
 const StyledDiv = styled.div `
-    background-color: red;
+    background-color: #99ffff;
     width: 30%;
     height: 100px;
 `
 
 const Converter = () => {
-    return (
+
+    function handleSubmit(e) {
+        alert('Хватит жрать!');
+        e.preventDefault();
+      }
+
+    return ( 
         <div>
             <StyledDiv>
-                <p className="hello">Hello</p>
+                <p className = "hello"> Hello </p> 
             </StyledDiv>
-            <NavLink to="/">Back</NavLink>
+            <NavLink to = "/" > Back </NavLink> 
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Food:
+                    <input type ="text" name="food"/> 
+                </label> 
+                <label>
+                    Weight(g):
+                    <input type ="text" name="Weight"/> 
+                </label> 
+                <label>
+                    Kkal:
+                    <input type ="text" name="Kkal"/> 
+                </label> 
+                <button type="submit">Рассчитать</button>
+            </form> 
+            
         </div>
     );
 };
